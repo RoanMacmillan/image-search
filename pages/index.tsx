c40a12566707a9e97a0edd5ebf857b8698867803
Bri Jackson <71437390+brijax@users.noreply.github.com>
2024-09-12 23:18:35 +0100

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
 import SearchComponent from "@/components/containers/search";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import React from "react";
 
 export default function Home() {
   return (
