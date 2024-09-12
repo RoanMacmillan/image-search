4bc2ebaf83e53032ab7670ce70068f6a20f2cd0e
Bri Jackson <71437390+brijax@users.noreply.github.com>
2024-09-12 23:07:35 +0100

    ..

--- components/containers/search.tsx
+++ components/containers/search.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Image from "next/image";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,21 @@
-import Image from "next/image";
 import localFont from "next/font/local";
 import SearchComponent from "@/components/containers/search";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export default function Home() {
   return (
-   
-    <div className="flex items-center justify-center min-h-screen">
-      
-
+    <div className="flex min-h-screen items-center justify-center">
       <SearchComponent></SearchComponent>
-
     </div>
-
-
   );
 }
